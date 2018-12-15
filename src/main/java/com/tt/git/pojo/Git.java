a5b319adda2466b49acd12746b7c462e146977d8
niuweimin <niuweimin0906@163.com>
2018-12-15 17:03:43 +0800

    创建一个name属性

--- src/main/java/com/tt/git/pojo/Git.java
+++ src/main/java/com/tt/git/pojo/Git.java
@@ -1,4 +1,13 @@
 package com.tt.git.pojo;
 
 public class Git {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
